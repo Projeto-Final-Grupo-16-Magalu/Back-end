016917300615a6b7d292667d0a292e3d82695e7c
Your Name <you@example.com>
2022-10-04 15:46:33 -0300

    Import da classe modelo de Cliente

--- games/rest/clientes_rest.py
+++ games/rest/clientes_rest.py
@@ -1,14 +1,14 @@
 from typing import List
 
-import clientes.regras.clientes_regras as clientes_regras
+import games.regras.clientes_regras as clientes_regras
 from fastapi import APIRouter, status
-from clientes.modelos import (ModeloBaseClientes,ModeloEmailClientes,
-                             ModeloGeralClientes)
+from games.modelos.cliente import (Cliente) 
+                             
 
-# Minha rota API de músicas
+# Minha rota API de clientes
 rota_clientes = APIRouter(
     # Prefixo para o caminho da rota
-    prefix="/api/musicas"
+    prefix="/api/clientes"
 )
 
 
@@ -17,20 +17,20 @@ rota_clientes = APIRouter(
     "/",
     # Ajustado o código HTTP de retorno
     status_code=status.HTTP_201_CREATED,
-    # Modelo da resposta
-    response_model=ModeloEmailClientes,
+    # Modelo da resposta *** original: response_model = ModeloCodigoMusica ***(codigo = mudei para "email")
+     response_model= Cliente, 
 )
-async def criar_novo_cliente(clientes: ModeloBaseClientes):
+async def criar_novo_cliente(clientes: Cliente):
     novo_cliente = await clientes_regras.inserir_novo_cliente(clientes)
     return novo_cliente
 
-# Atualiza a música pelo código. #*** atualiza cliente pelo email****
+# Atualiza a música pelo código. #*** atualiza cliente pelo email**** vamos usar id?
 @rota_clientes.put("/{email}")
 async def atualizar_cliente(email: str, cliente: dict):
     return None
 
 
-# Remove uma música pelo código *remove cliente pelo email
+# Remove cliente pelo email
 @rota_clientes.delete("/{email}")
 async def remover_cliente(email: str):
     return None
@@ -40,7 +40,7 @@ async def remover_cliente(email: str):
 @rota_clientes.get(
     "/{email}",
     # Informamos para a pesquisa o modelo da resposta
-    response_model=ModeloGeralClientes
+    response_model = Cliente #**original seria response_model=GeralMusica(com informaçoes da classe ModeloMusicaBase e ModeloCodigoMusica)
 )
 async def pesquisar_cliente_pelo_email(email: str):
     cliente = await clientes_regras.pesquisar_por_email(email, True)
@@ -51,8 +51,8 @@ async def pesquisar_cliente_pelo_email(email: str):
 @rota_clientes.get(
     "/",
     # E também informamos aqui o modelo da resposta.
-    response_model=List[ModeloGeralClientes]
+     response_model=List[Cliente] #**original seria [GeralMusica](com informaçoes da classe ModeloMusicaBase e ModeloCodigoMusica)
 )
-async def pesquisar_todos_os_clientes() -> List[ModeloGeralClientes]:
+async def pesquisar_todos_os_clientes() -> List[Cliente]:  #**original seria [GeralMusica](com informaçoes da classe ModeloMusicaBase e ModeloCodigoMusica)
     todos = await clientes_regras.pesquisar_por_todos()
     return todos
