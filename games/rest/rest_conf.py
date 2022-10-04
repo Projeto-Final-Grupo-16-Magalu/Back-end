a3234764f424f2289b3dc9ee0610413d23acea08
Your Name <you@example.com>
2022-10-04 11:57:14 -0300

    Adição da rota_clientes

--- games/rest/rest_conf.py
+++ games/rest/rest_conf.py
@@ -4,11 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from games.rest.principal_rest import rota_principal
 from games.rest.games_rest import rota_games
 from games.rest.carrinho_rest import rota_carrinho
+from games.rest.clientes_rest import rota_clientes
 
 def configurar_rotas(app: FastAPI):
     app.include_router(rota_principal)
     app.include_router(rota_games)
     app.include_router(rota_carrinho)
+    app.include_router(rota_clientes)
 
 ############  Meninas, essa conf do Cors é para receber o front se houver tempo ##################
 def configurar_api_rest(app: FastAPI):
