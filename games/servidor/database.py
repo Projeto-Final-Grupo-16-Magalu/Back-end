ca68f95dba78b36b6009b3b290e9cb80c72df062
Camila Cangussu <ccangussu@nelogica.com.br>
2022-10-04 21:57:08 -0300

    Adiciona suporte a banco de dados

---
+++ games/servidor/database.py
@@ -0,0 +1,31 @@
+from os import environ
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class DataBase:
+    client: AsyncIOMotorClient = None
+    database_uri = environ.get('DATABASE_URI')
+    colecao_clientes = None
+    colecao_enderecos = None
+    colecao_produtos = None
+    colecao_carrinhos = None
+
+db = DataBase()
+
+async def connect_db():
+    # conexao mongo, com no máximo 10 conexões async
+    db.client = AsyncIOMotorClient(
+        db.database_uri,
+        maxPoolSize=10,
+        minPoolSize=10,
+        tls=True,
+        tlsAllowInvalidCertificates=True
+    )
+    db.colecao_clientes = db.client.magalugames.clientes
+    db.colecao_enderecos = db.client.magalugames.enderecos
+    db.colecao_produtos = db.client.magalugames.produtos
+    db.colecao_carrinhos = db.client.magalugames.carrinhos
+
+async def disconnect_db():
+    db.client.close()
