638408d09dd5c1f5269ceaa224c418cc2e1b1417
Your Name <you@example.com>
2022-10-05 13:18:59 -0300

    Insercao da pasta de excecoes

--- games/regras/excecoes.py
+++ games/regras/excecoes.py
@@ -1,20 +1,14 @@
-"""
-Exceções das regras.
-"""
 class RegraExcecao(Exception):
-    # Exceção geral das regras
     def __init__(self, mensagem: str) -> None:
         super(RegraExcecao, self).__init__(mensagem)
         self.mensagem = mensagem
 
 
 class NaoEncontradoExcecao(RegraExcecao):
-    # Exceção geral para um registro não encontrado.
     def __init__(self, mensagem: str) -> None:
         super(NaoEncontradoExcecao, self).__init__(mensagem)
 
 
 class OutroRegistroExcecao(RegraExcecao):
-    # Exceção de que há outro registro coincidindo...
     def __init__(self, mensagem: str) -> None:
         super(OutroRegistroExcecao, self).__init__(mensagem)
