d1e4373910a491b6db3d744995f07d0045b93eba
Your Name <you@example.com>
2022-10-05 16:17:11 -0300

    Correcao conexao banco de dados enderecos

--- games/persistencia/enderecos.py
+++ games/persistencia/enderecos.py
@@ -4,19 +4,20 @@ from typing import List, Optional
 
 from pydantic import EmailStr
 from games.modelos.cliente import Cliente
-from games.servidor.database import (connect_db,disconnect_db)
+from games.servidor.database import (DataBase, connect_db)
 from games.modelos.endereco import Endereco, EnderecosCliente
 
 
 COLECAO_ENDERECOS = connect_db("enderecos")
-COLECAO_ENDERECOS = disconnect_db("enderecos")
+
+db = DataBase()
 
 async def pesquisar_pelo_email(email: EmailStr) -> Optional[dict]:
     filtro = {
         Cliente.email: email
     }
     enderecos = await COLECAO_ENDERECOS.find(filtro)
-    return enderecos
+    return db.colecao_enderecos
 
 async def pesquisar_todos() -> List[dict]:
     filtro = {}
