58cff7850cc453217eb29102909a7a1145751761
Your Name <you@example.com>
2022-10-06 19:21:35 -0300

    Correção das funções

--- games/persistencia/clientes.py
+++ games/persistencia/clientes.py
@@ -1,4 +1,4 @@
-
+    
 
 from typing import List, Optional
 from games.modelos.cliente import Cliente
--- games/persistencia/enderecos.py
+++ games/persistencia/enderecos.py
@@ -12,21 +12,13 @@ COLECAO_ENDERECOS = connect_db("enderecos")
 
 db = DataBase()
 
-async def pesquisar_pelo_email(email: EmailStr) -> Optional[dict]:
+async def pesquisar_endereço_por_email(email: EmailStr) -> Optional[dict]:
     filtro = {
         Cliente.email: email
     }
     enderecos = await COLECAO_ENDERECOS.find(filtro)
-    return db.colecao_enderecos
+    return db.colecao_enderecos 
 
-async def pesquisar_todos() -> List[dict]:
-    filtro = {}
-    cursor_pesquisa = COLECAO_ENDERECOS.find(filtro)
-    lista_todos = [
-       db.colecao_enderecos
-        async for enderecos in cursor_pesquisa
-    ]
-    return lista_todos
 
 async def inserir_um_novo_endereco(novo_endereco: dict) -> dict:
     await COLECAO_ENDERECOS.insert_one(novo_endereco)
