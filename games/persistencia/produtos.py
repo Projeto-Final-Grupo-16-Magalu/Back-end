5476706a07d88efbe5cf3d08fa99dccb2c5bd8fa
Caroline <2015302@aluno.univesp.br>
2022-10-12 01:03:08 -0300

    crud produto finalizado

--- games/persistencia/produtos.py
+++ games/persistencia/produtos.py
@@ -1,49 +1,57 @@
-# from motor.motor_asyncio import AsyncIOMotorClient
-# from games.servidor import database
-# from games.servidor.database import connect_db, db, disconnect_db
-
-   
-# async def cria_produto(produto):
-#     await connect_db()
-#     colecao_produtos = db.colecao_produtos
-   
-#     try:
-#         produto = await colecao_produtos.insert_one(produto)
-        
-#         #if produto.inserted_id:
-#            # produto = await cria_produto(colecao_produtos, produto.inserted_id)
-#         await  disconnect_db()   
-#         return produto.inserted_id
-
-#     except Exception as e:
-#         print(f'cria_produto.erro: {e}')
-
-
-from typing import List, Optional
-from games.modelos.produto import Produto
+from typing import Optional
+from games.modelos.produto import AtualizacaoProduto, Produto
 from games.servidor.database import obter_colecao
 from games.configuracoes import COLECAO_PRODUTOS
+from bson import ObjectId
 
+# 1.Cadastrar um produto
 async def inserir_novo_produto(produto: dict) -> dict: 
     colecao = obter_colecao(COLECAO_PRODUTOS)
     await colecao.insert_one(produto)
     return produto
 
 
-async def pesquisar_todos_produtos() -> List[dict]:
-    filtro = {}
-    cursor_pesquisa = obter_colecao(COLECAO_PRODUTOS).find(filtro)
-    lista_todos = [
-        produtos
-        async for produtos in cursor_pesquisa
-    ]
-    return lista_todos
-
-async def pesquisar_pelo_nome(nome: str) -> Optional[dict]:
+# 2.Atualizar os dados de um produto.
+async def atualizar_por_codigo(id : str, produto: AtualizacaoProduto) ->bool:
+    produto = { K: v for K, v in produto.dict().items() if v is not None}
+    
     filtro = {
-        "nome" : nome
+        '_id':ObjectId(id)
+    }
+    colecao = obter_colecao(COLECAO_PRODUTOS)
+
+    produto_atualizado = {
+        "$set": produto
+    }
+    alterado = await colecao.update_one(filtro, produto_atualizado)
+    return alterado
+
+
+
+# 3.Pesquisar um produto.
+async def pesquisar_pelo_id_produto(id):
+    filtro = {
+        '_id' : ObjectId(id)
     }
     colecao = obter_colecao(COLECAO_PRODUTOS)
     produto = await colecao.find_one(filtro)
-    
     return produto
+
+
+# 4.Pesquisar um produto pelo nome.
+async def pesquisar_pelo_nome(nome: str) -> Optional[dict]:
+    colecao = obter_colecao(COLECAO_PRODUTOS)
+    produto = await colecao.find_one({'nome' : nome})
+    return produto
+
+
+# 5.Remover um produto.
+async def delete_produto(id):
+    colecao = obter_colecao(COLECAO_PRODUTOS)
+    remover = await colecao.delete_one({'_id' : ObjectId(id)})
+    return remover
+
+
+
+
+ #PS:  import pdb; pdb.set_trace()
--- games/regras/produto.py
+++ games/regras/produto.py
@@ -1,12 +1,13 @@
+from fastapi import HTTPException
 from typing import List
 from games.configuracoes import COLECAO_PRODUTOS
 from games.modelos import produto
 import games.persistencia.produtos as produtos_persistencia
-from games.modelos.produto import Produto
-from games.regras.excecoes import OutroRegistroExcecao
+from games.modelos.produto import AtualizacaoProduto, Produto
+from games.regras.excecoes import NaoEncontradoExcecao, OutroRegistroExcecao
 from games.servidor.database import obter_colecao
 
-
+# VALIDAR A CRIAÇÃO DO PRODUTO
 async def inserir_novo_produto(produto: Produto) -> Produto:
     await validar_produto(produto)
     novo_produto = produto.dict()
@@ -17,97 +18,47 @@ async def inserir_novo_produto(produto: Produto) -> Produto:
     produto_geral = Produto(**novo_produto)
     return produto_geral
 
-async def pesquisar_todos_produtos()-> List[dict]:
-    todos = await produtos_persistencia.pesquisar_todos_produtos
-
 async def validar_produto(novo_produto: Produto):
+    
     outro_produto = await produtos_persistencia.pesquisar_pelo_nome(novo_produto.nome)
     if outro_produto is not None:
-       raise OutroRegistroExcecao("Há outra produto com este nome")
+        raise HTTPException(status_code=409, detail=f'Há outra produto com este nome')  
 
+# O status de resposta 409 Conflict indica que a solicitação atual conflitou com o recurso que está no servidor.
 
-
-
-
-# async def pesquisa_produto_pelo_codigo(
-#     codigo: str, lanca_excecao_se_nao_encontrado: bool = False
-# ) -> Optional[dict]:
-#     # Pesquisando pelo código no banco
-#     produto = await produto.pesquisa_produto_pelo_codigo(codigo)
-#     # Não encontrei, devo lançar exceção?
-#     if not produto and lanca_excecao_se_nao_encontrado:
-#         raise NaoEncontradoExcecao("Produto não encontrado")
-#     # Retornando o registro do banco.
-#     return produto
-
-
-# async def pesquisa_produtos() -> List[dict]:
-#     todos_produtos = await produto.pesquisa_produtos()
-#     return todos_produtos
-
-
-
-
-
-# async def remover_por_codigo(codigo: str):
+# ATUALIZAR
+async def atualizar_por_codigo(codigo: str, produto: AtualizacaoProduto):
+    
+        atualizar_produto = await produtos_persistencia.atualizar_por_codigo(codigo,produto)
+            
+        if atualizar_produto.modified_count:
+            return{'status': 'Produto atualizado com sucesso!'}
+        else: 
+           raise HTTPException(status_code=404, detail=f'Produto não encontrado')   
   
-#     removeu = await produto.remover_uma_produto_pelo_codigo(codigo)
+    
 
-#     if not removeu:
-#         raise NaoEncontradoExcecao("Produto não encontrado")
-
-
-# async def atualiza_produto(codigo: str, produto: Produto):
-#     # Pesquisando produto para atualizar
-#     # Se não existe, lançamos a exceção.
-#     await atualiza_produto(codigo, True)
-
-#     # Se foi informado o código, vamos ver se não são diferentes
-#     if produto.codigo is not None and produto.codigo != codigo:
-#         # Sim na linha de abaixo não esquecemos dos parêntesis.
-#         # Por quê?
-#         raise CodigosDiferentesExcecao
-
-#     # Validando, com código; para identificar que é uma atualização
-#     validar_produto(produto, codigo)
-
-#     produto_para_banco = produto.dict()
-#     # Pequeno ajuste, se o código não existe, devemos retirá-lo
-#     if produto.codigo is None:
-#         produto_para_banco.pop("CAMPO_CODIGO", None)
-
-#     # Atualizando no banco de dado
-#     await produto.atualizar_uma_produto_pelo_codigo(
-#         codigo, produto_para_banco
-#     )
+# PESQUISAR ID
+async def validar_id_produto(id: str):
+    id_produto = await produtos_persistencia.pesquisar_pelo_id_produto(id)
+    if not id_produto:
+       raise NaoEncontradoExcecao("Não há  produto com este id")
+    else: 
+        return(id_produto)
 
 
 
 
-# """
-# Exceções das regras.
-# """
 
 
-# class RegraExcecao(Exception):
-#     # Exceção geral das regras
-#     def __init__(self, mensagem: str) -> None:
-#         super(RegraExcecao, self).__init__(mensagem)
-#         self.mensagem = mensagem
 
 
-# class NaoEncontradoExcecao(RegraExcecao):
-#     # Exceção geral para um registro não encontrado.
-#     def __init__(self, mensagem: str) -> None:
-#         super(NaoEncontradoExcecao, self).__init__(mensagem)
-
-
-# class OutroRegistroExcecao(RegraExcecao):
-#     # Exceção de que há outro registro coincidindo...
-#     def __init__(self, mensagem: str) -> None:
-#         super(OutroRegistroExcecao, self).__init__(mensagem)
-
-# # 
-# class CodigosDiferentesExcecao(OutroRegistroExcecao):
-#     def __init__(self) -> None:
-#         super().__init__("Código diferentes")  
+#DELETAR
+async def delete_produto(id: str):
+   
+    produto_deletado = await produtos_persistencia.delete_produto(id)
+    
+    if produto_deletado.deleted_count:
+        return{'status': 'Produto deletado com sucesso!'}
+    else: 
+        return{ 'status': 'Produto inexistente'}    
